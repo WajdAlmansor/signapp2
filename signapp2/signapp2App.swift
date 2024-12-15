8e28da5238e47c2ee68221317d04dcb821c01e9e
WajdButterfly <wajd2828@gmail.com>
2024-12-15 14:52:35 +0300

    Initial Commit

---
+++ signapp2/signapp2App.swift
@@ -0,0 +1,17 @@
+//
+//  signapp2App.swift
+//  signapp2
+//
+//  Created by Wajd on 15/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct signapp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
